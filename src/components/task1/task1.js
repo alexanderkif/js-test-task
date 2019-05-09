11d1b7f44343ab210dda247d19741e8c2d181449
alexanderkif <alexander_kif@mail.ru>
2019-05-09 07:03:02 +0400

    labels moved to json, added onenter

--- src/components/task1/task1.js
+++ src/components/task1/task1.js
@@ -1,36 +1,38 @@
 import { bind } from 'decko';
-import { isNumber } from 'util';
 
 class Task1 {
     constructor(element) {
         this.number1 = element.querySelector('.js-task1__number1');
-        this.number1.addEventListener("keyup", this.checkInput);
+        this.number1.addEventListener('keyup', this.checkInput);
         this.number2 = element.querySelector('.js-task1__number2');
-        this.number2.addEventListener("keyup", this.checkInput);
+        this.number2.addEventListener('keyup', this.checkInput);
         this.btn = element.querySelector('.js-task1__submit');
-        this.btn.addEventListener("click", this.summ);
+        this.btn.addEventListener('click', this.summ);
     }
 
     @bind
     summ() {
-        alert('Result:\n' + (+this.number1.value + +this.number2.value));
+        alert(`Result:\n${+this.number1.value + +this.number2.value}`);
     }
 
     @bind
     checkInput(event) {
-        if(isNaN(event.target.value)) {
+        if (event.target.value === '' || isNaN(event.target.value)) {
             event.target.classList.add('error');
             this.btn.disabled = true;
+        } else {
+            event.target.classList.remove('error');
         }
-        else event.target.classList.remove('error');
 
-        if(isNaN(this.number1.value) || isNaN(this.number2.value))
+        if (this.number1.classList.contains('error')
+            || this.number2.classList.contains('error')) {
             this.btn.disabled = true;
-        else
+        } else {
             this.btn.disabled = false;
+        }
+
+        if (event.keyCode === 13 && !this.btn.disabled) this.summ();
     }
 }
 
-[].forEach.call(document.getElementsByClassName('task1'), element => {
-    new Task1(element);
-});
+[].forEach.call(document.getElementsByClassName('task1'), element => new Task1(element));
